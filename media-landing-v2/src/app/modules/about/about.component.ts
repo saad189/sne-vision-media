c0828262e0451864343f8f8bb3b7026ebb17360e
Saad Ahmed <saad18910@hotmail.com>
2025-11-03 10:43:32 +0500

    Added Employee Images

--- media-landing-v2/src/app/modules/about/about.component.ts
+++ media-landing-v2/src/app/modules/about/about.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about',
-    standalone: false,
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss']
+  selector: 'app-about',
+  standalone: false,
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
+  // Interface for clarity; could be moved to a shared models folder if reused elsewhere
+  teamMembers: TeamMember[] = [
+    {
+      image: 'assets/images/team/f1.png',
+      name: 'M. Saad Ahmed',
+      role: 'Co-Founder & CEO',
+      quote: 'Driving innovation and shaping the future of visionary media.',
+    },
+    {
+      image: 'assets/images/team/f2.png',
+      name: 'M. Fahad Nasir',
+      role: 'Co-Founder',
+      quote: 'Building scalable technology that empowers creative minds.',
+    },
+    {
+      image: 'assets/images/team/e1.png',
+      name: 'Aroob Fatima',
+      role: 'Research Analyst',
+      quote: '',
+    },
+  ];
+}
 
+interface TeamMember {
+  image: string;
+  name: string;
+  role: string;
+  quote: string;
 }
